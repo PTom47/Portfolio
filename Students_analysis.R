741ff24f4421c6a3b4dcdf1fb1066b10d455eaf9
PTom47 <piotr.tomczak.edu@gmail.com>
2023-04-24 18:13:37 +0200

    Update Students_analysis.R

--- Students_analysis.R
+++ Students_analysis.R
@@ -3,6 +3,9 @@
 library(RODBC)
 library(DBI)
 library(odbc)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
 con <- dbConnect(odbc(), Driver = "SQL Server", Server = "localhost\\SQLEXPRESS", 
                  Database = "TEST", Trusted_Connection = "True")
 Student_Performance <- dbGetQuery(con, "select * from [TEST].[dbo].[Student_Performance]")
@@ -27,21 +30,8 @@ apply(Student_Performance[25:35], 2, mean)
 apply(Student_Performance[25:35], 2, median)
 
 #Wizualizacja średnich
-mean_apl <-  apply(Student_Performance[25:35], 2, mean)
-library(tidyverse)
-mean_df <- tibble(lab = names(sum_apl), mean_d = mean_apl)
-
-#wektor pomocniczy do ustalenia rosnącej kolejności na wykresie
-et1 <- c(arrange(mean_df, mean_d)$lab)
-
-
-#wykres średnich
-library(ggplot2)
-ggplot(data = mean_df)+
-  geom_point(aes(x=lab, y=mean_d))+
-  scale_x_discrete(limits=et1)+
-  labs(title = "Student Performance mean")+
-  theme_bw()
+mean_apl <-  data.frame(avg_value=apply(Student_Performance[25:35], 2, mean))
+mean_apl$category <- row.names(mean_apl)
 
 #wykres zależności G1 i G2
 ggplot(Student_Performance,aes(G1, G2))+
@@ -49,13 +39,12 @@ ggplot(Student_Performance,aes(G1, G2))+
   labs(title = "Student Performance, G1&G2")+
   theme_light()
 
-#Mjob & internet
+#wykres Mother job & internet
 Mjob_int <- data.frame(table(Student_Performance$Mjob, Student_Performance$internet)) 
 Mjob_int_y <- Mjob_int[Mjob_int$Var2=="yes",]
 
 et2 <- c(arrange(Mjob_int_y, Freq)$Var1)
 
-#wykres Mjob & internet
 ggplot(Mjob_int_y, aes(Var1, Freq))+
          geom_point()+
          scale_x_discrete(limits=et2)+
@@ -63,6 +52,15 @@ ggplot(Mjob_int_y, aes(Var1, Freq))+
                x = "Mother job",
                y = "Freq")+
           theme_minimal()
-               
+
+#wykres Mother job & studytime               
+ggplot(Student_Performance, aes(x=Mjob, y=studytime))+
+  geom_bar(stat="summary", fill = "cornflowerblue")+
+  stat_summary(fun.data = mean_se, geom = "errorbar")+
+  labs(title = "Studytime",
+       x = "Mother job",
+       y = "studytime")+
+  theme_minimal()
+
 
 
